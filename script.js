eb18ed642adc01d65cadcae18556e26287c54e3e
MBoksh <mikaaeelboksh@hotmail.com>
2022-10-31 14:47:09 +0000

    schedulecontainers functions to save inputs

--- script.js
+++ script.js
@@ -54,3 +54,45 @@ let schedule =
         tasks: ""
     }
 ];
+
+$(document).ready(function()
+{
+    
+    loadtasks();
+
+    
+    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+    var scheduleContainer = $("#scheduleContainer"); 
+    var getCurrentHour = (moment().format('H'));  
+    $.each(schedule, function(i, time)  
+    {
+        scheduleContainer.append("<div id=\"time" + i + "\"" + "class=\"row\"></div>"); 
+        $("#time"+i).append("<div id=\"currentTime" + i + "\"" + "class=\"col-2 hour\">" + time.Time + "</div>"); 
+        let scheduleHour = time.tag; 
+        
+            var stateClass = ""; 
+            if (getCurrentHour > scheduleHour) 
+            {
+                stateClass ="past";
+            }
+            else if (getCurrentHour == scheduleHour)  
+            {
+                stateClass ="present";
+            }
+            else if (getCurrentHour < scheduleHour)  
+            {
+                stateClass ="future";
+            } 
+        $("#time"+i).append("<textarea id=\"textArea" + i + "\"" + "class=\"col-8 " + stateClass + "\"textarea\">" + schedule[i].tasks + "</textarea>");  
+        $("#time"+i).append("<div id=\"saveBtn" + i + "\"" + "data-index=\"" + i + "\"" + "class=\"col-2 saveBtn\">" + "save" + "</div>");  
+    });
+
+    
+    $(".saveBtn").click(function(event) 
+    {
+        var element = event.target; 
+        var index = parseInt($(element).attr("data-index"),10); 
+        saveTask(index); 
+    });
+});
