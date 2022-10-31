1d83c77585952bf53daa2e27f21a80ac32f0284a
MBoksh <mikaaeelboksh@hotmail.com>
2022-10-31 14:39:02 +0000

    made objects array for time schedule

---
+++ script.js
@@ -0,0 +1,56 @@
+let schedule = 
+[
+    {
+        Time: "9 am", 
+        tag: 9,  
+        tasks: ""  
+    },
+
+    {
+        Time: "10 am",
+        tag: 10,
+        tasks: ""
+    },
+
+    {
+        Time: "11 am",
+        tag: 11,
+        tasks: ""
+    },
+
+    {
+        Time: "12 pm",
+        tag: 12,
+        tasks: ""
+    },
+
+    {
+        Time: "1 pm",
+        tag: 13,
+        tasks: ""
+    },
+
+    {
+        Time: "2 pm",
+        tag: 14,
+        tasks: ""
+    },
+
+    {
+        Time: "3 pm",
+        tag: 15,
+        tasks: ""
+    },
+
+    {
+        Time: "4 pm",
+        tag: 16,
+        tasks: ""
+    },
+
+    {
+        Time: "5 pm",
+        tag: 17,
+        tasks: ""
+    }
+];
